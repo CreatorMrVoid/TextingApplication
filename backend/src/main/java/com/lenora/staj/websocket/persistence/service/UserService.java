bd927dac0777c07fb2ef8f29b26e769b785a3191
Özgün Soykök <157131264+CreatorMrVoid@users.noreply.github.com>
2024-06-27 09:58:42 +0300

    Backend Fix
    26.06.2024 backend fix

--- backend/src/main/java/com/lenora/staj/websocket/persistence/service/UserService.java
+++ backend/src/main/java/com/lenora/staj/websocket/persistence/service/UserService.java
@@ -3,6 +3,7 @@ package com.lenora.staj.websocket.persistence.service;
 import at.favre.lib.crypto.bcrypt.BCrypt;
 import com.lenora.staj.websocket.persistence.model.User;
 import com.lenora.staj.websocket.persistence.repository.UserRepository;
+import org.apache.commons.lang3.StringUtils;
 import org.hibernate.boot.model.source.spi.Sortable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,15 +20,20 @@ public class UserService {
         User user = new User();
         user.setUsername(username);
         String hashedPassword = BCrypt.withDefaults().hashToString(12, password.toCharArray());
-        user.setPassword(hashedPassword); // Ideally, the password should be hashed
+        user.setPassword(hashedPassword);
         return userRepository.save(user);
-
     }
 
+    /**
+     * Finds the user via given username, verifies the password and returns the user if found, or null otherwise.
+     * @param username user to login as
+     * @param password password of user
+     * @return User object or null
+     */
     public User loginUser(String username, String password) {
         User user = userRepository.findByUsername(username);
 
-        if (user != null) {
+        if (user != null && StringUtils.isNotBlank(password)) {
             BCrypt.Result result = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword().toCharArray());
             if (result.verified) {
                 return user;
---
+++ backend/src/main/java/com/lenora/staj/websocket/rest/request/UserPassView.java
@@ -0,0 +1,9 @@
+package com.lenora.staj.websocket.rest.request;
+
+import lombok.Data;
+
+@Data
+public class UserPassView {
+    String username;
+    String password;
+}
